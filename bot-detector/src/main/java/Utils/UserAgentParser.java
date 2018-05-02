68a9f7185165b4b2a0e28a2a7ed42a0cf3e307b3
Nikita Suprotivniy <n.suprotivniy@corp.mail.ru>
2018-05-02 13:16:33 +0300

    Выносит парсер user_agent в broadcast переменную.

--- bot-detector/src/main/java/Utils/UserAgentParser.java
+++ bot-detector/src/main/java/Utils/UserAgentParser.java
@@ -2,6 +2,8 @@ package Utils;
 
 import nl.basjes.parse.useragent.UserAgent;
 import nl.basjes.parse.useragent.UserAgentAnalyzer;
+import org.apache.spark.SparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -9,10 +11,16 @@ import org.apache.spark.sql.api.java.UDF1;
 import org.apache.spark.sql.types.DataTypes;
 
 public class UserAgentParser {
-    public static UserAgentAnalyzer uaa;
-
     public static void main(String[] args) {
-        uaa = UserAgentAnalyzer
+        if (args.length < 2) {
+            System.err.println("arguments: <path_to_input_parquet> <<path_to_output_parquet>>");
+            return;
+        }
+
+        String inParquet = args[0];
+        String outParquet = args[1];
+
+        UserAgentAnalyzer uaa = UserAgentAnalyzer
                 .newBuilder()
                 .hideMatcherLoadStats()
                 .withCache(25000)
@@ -22,13 +30,20 @@ public class UserAgentParser {
                 .appName("UserAgentParser")
                 .master("local[*]")
                 .getOrCreate();
-        System.out.println(args);
-        Dataset<Row> df = spark.read().parquet(args[0]);
+
+        SparkContext sparkContext = spark.sparkContext();
+        Broadcast<UserAgentAnalyzer> uaaBroadcast = sparkContext.broadcast(uaa, scala.reflect.ClassManifestFactory.fromClass(UserAgentAnalyzer.class));
+
+        Dataset<Row> df = spark.read().parquet(inParquet);
+        int partitions = df.rdd().getNumPartitions();
+        df.printSchema();
+        System.out.println("Count: " + df.count());
 
         UDF1 parseUserAgent = new UDF1<String, String>() {
             public String call(final String userAgentString) throws Exception {
-                UserAgent agent = uaa.parse(userAgentString);
+                UserAgentAnalyzer uaa = uaaBroadcast.getValue();
                 StringBuilder stringBuilder = new StringBuilder();
+                UserAgent agent = uaa.parse(userAgentString);
                 for (String fieldName: agent.getAvailableFieldNamesSorted()) {
                     stringBuilder.append(agent.getValue(fieldName));
                     stringBuilder.append(";:;");
@@ -39,30 +54,33 @@ public class UserAgentParser {
         };
 
         spark.udf().register("parseUserAgent", parseUserAgent, DataTypes.StringType);
-        df.show(1, false);
-        df = df.selectExpr("browserId", "from", "method", "operation", "referrer", "requestType", "timestamp", "to", "url", "parseUserAgent(userAgent) as userAgent", "userId", "hour");
-        df.selectExpr("browserId", "from", "method", "operation", "referrer", "requestType", "timestamp", "to", "url", "split(userAgent, ';:;')[0] as DeviceClass",
-                "split(userAgent, ';:;')[1] as DeviceName",
-                "split(userAgent, ';:;')[2] as DeviceBrand",
-                "split(userAgent, ';:;')[3] as DeviceCpu",
-                "split(userAgent, ';:;')[4] as DeviceCpuBits",
-                "split(userAgent, ';:;')[5] as OperatingSystemClass",
-                "split(userAgent, ';:;')[6] as OperatingSystemName",
-                "split(userAgent, ';:;')[7] as OperatingSystemVersion",
-                "split(userAgent, ';:;')[8] as OperatingSystemNameVersion",
-                "split(userAgent, ';:;')[9] as OperatingSystemVersionBuild",
-                "split(userAgent, ';:;')[10] as LayoutEngineClass",
-                "split(userAgent, ';:;')[11] as LayoutEngineName",
-                "split(userAgent, ';:;')[12] as LayoutEngineVersion",
-                "split(userAgent, ';:;')[13] as LayoutEngineVersionMajor",
-                "split(userAgent, ';:;')[14] as LayoutEngineNameVersion",
-                "split(userAgent, ';:;')[15] as LayoutEngineNameVersionMajor",
-                "split(userAgent, ';:;')[16] as AgentClass",
-                "split(userAgent, ';:;')[17] as AgentName",
-                "split(userAgent, ';:;')[18] as AgentVersion",
-                "split(userAgent, ';:;')[19] as AgentVersionMajor",
-                "split(userAgent, ';:;')[20] as AgentNameVersion",
-                "split(userAgent, ';:;')[21] as AgentNameVersionMajor",
-                "userId", "hour").write().format("parquet").save("parsedUserAgent.parquet");
+
+        df = df.selectExpr("*", "parseUserAgent(userAgent) as parsedUserAgent")
+                .selectExpr("*",
+                    "split(parsedUserAgent, ';:;')[0] as DeviceClass",
+                    "split(parsedUserAgent, ';:;')[1] as DeviceName",
+                    "split(parsedUserAgent, ';:;')[2] as DeviceBrand",
+                    "split(parsedUserAgent, ';:;')[3] as DeviceCpu",
+                    "split(parsedUserAgent, ';:;')[4] as DeviceCpuBits",
+                    "split(parsedUserAgent, ';:;')[5] as OperatingSystemClass",
+                    "split(parsedUserAgent, ';:;')[6] as OperatingSystemName",
+                    "split(parsedUserAgent, ';:;')[7] as OperatingSystemVersion",
+                    "split(parsedUserAgent, ';:;')[8] as OperatingSystemNameVersion",
+                    "split(parsedUserAgent, ';:;')[9] as OperatingSystemVersionBuild",
+                    "split(parsedUserAgent, ';:;')[10] as LayoutEngineClass",
+                    "split(parsedUserAgent, ';:;')[11] as LayoutEngineName",
+                    "split(parsedUserAgent, ';:;')[12] as LayoutEngineVersion",
+                    "split(parsedUserAgent, ';:;')[13] as LayoutEngineVersionMajor",
+                    "split(parsedUserAgent, ';:;')[14] as LayoutEngineNameVersion",
+                    "split(parsedUserAgent, ';:;')[15] as LayoutEngineNameVersionMajor",
+                    "split(parsedUserAgent, ';:;')[16] as AgentClass",
+                    "split(parsedUserAgent, ';:;')[17] as AgentName",
+                    "split(parsedUserAgent, ';:;')[18] as AgentVersion",
+                    "split(parsedUserAgent, ';:;')[19] as AgentVersionMajor",
+                    "split(parsedUserAgent, ';:;')[20] as AgentNameVersion",
+                    "split(parsedUserAgent, ';:;')[21] as AgentNameVersionMajor")
+                .drop("parsedUserAgent");
+
+        df.repartition(partitions).write().mode("overwrite").format("parquet").save(outParquet);
     }
 }
